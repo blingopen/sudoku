8828c57bc1daddab1e1181f941d12075b7f6caf4
blingopen <792419745@qq.com>
2018-12-16 22:31:14 +0800

    新增详细设计，代码更新--已完成正确性要求

--- Mysudoku/MySudoku/Program.cs
+++ Mysudoku/MySudoku/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Threading;
 
 namespace MySudoku
 {
@@ -14,58 +15,97 @@ namespace MySudoku
             System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
             sw.Start();
 
-
-
             Sudoku sudoku = new Sudoku();
-            sudoku.GenerateSudokuEnding();
-            
+
             string fileName = "sudoku.txt";
             string newPath = System.AppDomain.CurrentDomain.BaseDirectory + fileName;
-            Console.WriteLine(newPath);
             StreamWriter streamWriter = new StreamWriter(newPath, false, Encoding.Default);
+
 
             int parseInt = 0;
             int amount = 0;
-
+            Console.WriteLine(args[1].ToString());
             if (args.Length == 2)
             {
-                if(args[0] == "-c")
+                if (args[0] == "-c")
                 {
-                    if(int.TryParse(args[1],out parseInt))
+                    if (int.TryParse(args[1], out parseInt))
                     {
                         amount = parseInt;
                         while (amount > 0)
                         {
-                            OutputToTxt(sudoku,streamWriter);
+                            sudoku.Initialize();
+                            sudoku.GenerateSudokuEnding();
+                            //GetAnswer(0, sudoku1);
+                            OutputToTxt(sudoku, streamWriter);
                             amount--;
-                            Console.WriteLine("{0}", amount);
+                            //Console.WriteLine("{0}", amount);
                         }
                     }
                     else
                     {
-                            Console.WriteLine("不合法输入");
+                        Console.WriteLine("不合法输入");
                     }
                 }
                 else if (args[0] == "-s")
                 {
-                    //InputSudoku(sudoku);
-                    sudoku.SovleSudoku();
-                    Console.WriteLine("这个功能还没做！");
+                    string path = args[1];
+                    string allTheText = "";
+                    allTheText = InputSudoku(allTheText, args[1]);
+
+                    string[] all = allTheText.Split(new char[4] { ' ', '\n', '\r', '\0' });
+
+                    int allLength = all.Length;
+
+                    for (int i = 0; i < all.Length;)
+                    {
+                        sudoku.Initialize();
+                        int j = 0;
+                        for (j = 0; j < 81; j++)
+                        {
+                            int hang = j / 9, lie = j % 9;
+                            if (i < all.Length)
+                            {
+                                if (all[i] == "" || all[i] == "\n" || all[i] == "\0" || all[i] == "\r")
+                                {
+                                    j--;
+                                    i++;
+                                    continue;
+                                }
+                                sudoku.Shudu[hang, lie] = int.Parse(all[i]);
+                                if (int.Parse(all[i]) != 0)
+                                {
+                                    sudoku.Hang[hang, int.Parse(all[i]) - 1] = true;
+                                    sudoku.Lie[lie, int.Parse(all[i]) - 1] = true;
+                                    sudoku.Sansan[hang / 3 * 3 + lie / 3, int.Parse(all[i]) - 1] = true;
+                                }
+                            }
+                            else
+                                break;
+                            i++;
+                        }
+                        if (j == 81)
+                        {
+                            sudoku.SolveSudoku(0);
+                            OutputToTxt(sudoku, streamWriter);
+                        }
+                    }
                 }
-            }
-            else
-            {
-                Console.WriteLine("命令行没有参数！或参数个数不正确！");
-            }
+                else
+                {
+                    Console.WriteLine("命令行没有参数！或参数个数不正确！");
+                }
 
-            streamWriter.Flush();
-            streamWriter.Close();
+                streamWriter.Flush();
+                streamWriter.Close();
 
-            sw.Stop();
-            TimeSpan ts2 = sw.Elapsed;
-            Console.WriteLine("花费{0}", ts2.TotalSeconds);
+                sw.Stop();
+                TimeSpan ts2 = sw.Elapsed;
+                Console.WriteLine("花费{0}", ts2.TotalSeconds);
+            }
         }
-       static void OutputToTxt(Sudoku sudoku,StreamWriter streamWriter)
+
+        static void OutputToTxt(Sudoku sudoku, StreamWriter streamWriter)
         {
             for (int i = 0; i < 9; i++)
             {
@@ -84,9 +124,81 @@ namespace MySudoku
             streamWriter.WriteLine();
         }
 
-       //static Sudoku InputSudoku(Sudoku sudoku)
-        //{
-            
-       // }
+        static string InputSudoku(string allTheText, string path)
+        {
+            if (File.Exists(path))
+            {
+                StreamReader sr = File.OpenText(path);
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    allTheText += line;
+                    allTheText += " ";
+                }
+                return allTheText;
+                //Console.WriteLine(allTheText);
+            }
+            else
+            {
+                Console.WriteLine("数独文件不存在！");
+                return "";
+            }
+        }
+
+        static int GetRandomSeed()
+        {
+            byte[] bytes = new byte[10];
+            System.Security.Cryptography.RNGCryptoServiceProvider rng = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            rng.GetBytes(bytes);
+            return BitConverter.ToInt32(bytes, 0);
+        }
+
+        public static void Random159(int[] gene)
+        {
+            //Thread.Sleep(1);
+            Random rand = new Random(GetRandomSeed());
+            for (int i = gene.Length - 1; i > 0; i--)
+            {
+                int rdmchar = rand.Next(1000) % i;
+                int temp = gene[rdmchar];
+                gene[rdmchar] = gene[i];
+                gene[i] = temp;
+            }
+        }
+
+        /*static public bool Solve(Sudoku sudoku, int position)
+        {
+            int index = position;
+            for (; index < 81 && 0 != sudoku.Shudu[index / 9,index % 9]; index++) ;
+
+            if (index < 81)
+            {
+                int line = index / 9;
+                int col = index % 9;
+                for (int i = 1; i < 10; i++)
+                {
+                    if (sudoku.Hang[line,i - 1] || sudoku.Lie[col,i - 1] || sudoku.Sansan[line / 3 * 3 + col / 3,i - 1])
+                        continue;
+
+                    sudoku.Shudu[line,col] = i;
+                    sudoku.Hang[line,i - 1] = true;
+                    sudoku.Lie[col,i - 1] = true;
+                    sudoku.Sansan[line / 3 * 3 + col / 3,i - 1] = true;
+
+                    if (Solve(sudoku, index + 1))
+                        return true;
+
+                    sudoku.Shudu[line, col] = 0;
+                    sudoku.Hang[line, i - 1] = false;
+                    sudoku.Lie[col, i - 1] = false;
+                    sudoku.Sansan[line / 3 * 3 + col / 3, i - 1] = false;
+                }
+                return false;
+            }
+            return true;
+        }
+        */
     }
+
+
 }
--- Mysudoku/MySudoku/Sudoku.cs
+++ Mysudoku/MySudoku/Sudoku.cs
@@ -10,33 +10,106 @@ namespace MySudoku
     {
         public void GenerateSudokuEnding()
         {
-            //替换部分
+            int[] gene59 = new int[9] { 9,5,2,7,6,1,3,8,4 };
+            int[] gene1 = new int[8] { 5,2,7,3,6,8,1,4 };
+            Generate33(gene1, 1);
+            Generate33(gene59, 5);
+            Generate33(gene59, 9);
+            SolveSudoku(0);
+        }
+
+        public void Initialize()
+        {
             for (int i = 0; i < 9; i++)
             {
                 for (int j = 0; j < 9; j++)
                 {
-                    shudu[i, j] = Convert.ToChar(i+65);
+                    shudu[i, j] = 0;
+                    hang[i, j] = false;
+                    lie[i, j] = false;
+                    sansan[i, j] = false;
                 }
+            }
+            shudu[0, 0] = 9;
+            hang[0, 9 - 1] = true;
+            lie[0, 9 - 1] = true;
+            sansan[0, 9 - 1] = true;
+        }
+
+        public bool SolveSudoku(int position)
+        {
+            int index = position;
+            for (; index < 81 && 0 != shudu[index / 9, index % 9]; index++) ;
+
+            if (index < 81)
+            {
+                int line = index / 9;
+                int col = index % 9;
+                for (int i = 1; i < 10; i++)
+                {
+                    if (hang[line, i - 1] || lie[col, i - 1] || sansan[line / 3 * 3 + col / 3, i - 1])
+                        continue;
+
+                    shudu[line, col] = i;
+                    hang[line, i - 1] = true;
+                    lie[col, i - 1] = true;
+                    sansan[line / 3 * 3 + col / 3, i - 1] = true;
+
+                    if (SolveSudoku(index + 1))
+                        return true;
+
+                    shudu[line, col] = 0;
+                    hang[line, i - 1] = false;
+                    lie[col, i - 1] = false;
+                    sansan[line / 3 * 3 + col / 3, i - 1] = false;
+                }
+                return false;
+            }
+            return true;
+        }
+
+        private void Generate33(int[] gene, int num)
+        {
+            int temp = 0;
+            int row;
+            switch (num)
+            {
+                case 1: { row = 1; temp = 1; } break;
+                case 5: row = 4; break;
+                default: row = 7; break;
+            }
+            Program.Random159(gene);
+            for (int i = 0; i < gene.Length; i++)
+            {
+                if (i + temp < 3)
+                {
+                    shudu[row - 1, row / 3 * 3 + (i + temp) % 3] = gene[i];
+                    hang[row - 1, gene[i] - 1] = true;
+                }
+                else if (i + temp < 6)
+                {
+                    shudu[row, row / 3 * 3 + (i + temp) % 3] = gene[i];
+                    hang[row, gene[i] - 1] = true;
+                }
+                else
+                {
+                    shudu[row + 1, row / 3 * 3 + (i + temp) % 3] = gene[i];
+                    hang[row + 1, gene[i] - 1] = true;
+                }
+
+                lie[row / 3 * 3 + (i + temp) % 3, gene[i] - 1] = true;
+                sansan[num - 1, gene[i] - 1] = true;
             }
         }
 
-        public void SovleSudoku()
-        {
-            //填充部分
-
-            Console.WriteLine("求解完成");
-        }
-
-        private char[,] shudu = new char[9, 9];
-
-        public char[,] Shudu
+        private int[,] shudu = new int[9, 9];
+        public int[,] Shudu
         {
             get { return shudu; }
             set { shudu = value; }
         }
 
         private bool[,] hang = new bool[9, 9];
-
         public bool[,] Hang
         {
             get { return hang; }
@@ -44,7 +117,6 @@ namespace MySudoku
         }
 
         private bool[,] lie = new bool[9, 9];
-
         public bool[,] Lie
         {
             get { return lie; }
@@ -52,12 +124,27 @@ namespace MySudoku
         }
 
         private bool[,] sansan = new bool[9, 9];
-
         public bool[,] Sansan
         {
             get { return sansan; }
             set { sansan = value; }
         }
-       
-     }
+
+        public Sudoku()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    shudu[i, j] = 0;
+                }
+            }
+            shudu[0, 0] = 9;
+            hang[0, 9 - 1] = true;
+            lie[0, 9 - 1] = true;
+            sansan[0, 9 - 1] = true;
+        }
+
+    }
+
 }
